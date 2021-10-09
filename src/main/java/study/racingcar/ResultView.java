7ccbab0c494a69b500333aa73fc72f9f7b83cffd
khj1 <74335027+khj1@users.noreply.github.com>
2021-10-09 22:37:28 +0900

    feat : Result UI 구현
    요구사항 내용:
        - ResultView 객체를 통해 결과 출력 UI를 구현

---
+++ src/main/java/study/racingcar/ResultView.java
@@ -0,0 +1,28 @@
+package study.racingcar;
+
+public class ResultView {
+    public static void printInit() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printResults(Cars cars) {
+        cars.getCars().forEach(ResultView::printResult);
+    }
+
+    private static void printResult(Car car) {
+        String position = convertPositionToDash(car.getPosition());
+        System.out.println(car.getName() + " : " + position);
+    }
+
+    private static String convertPositionToDash(Integer position) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
+    public void printWinners(PlayResult result) {
+        System.out.println(result + "가 최종 우승했습니다.");
+    }
+}
