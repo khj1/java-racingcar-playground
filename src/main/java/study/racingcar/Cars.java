5d9f9ae939730f3ce7195107f97638c301587b94
khj1 <74335027+khj1@users.noreply.github.com>
2021-10-09 18:56:24 +0900

    feat : 요구사항 3 구현
    요구사항 내용:
        - 자동차 이름은 쉼표를 기준으로 구분한다.

---
+++ src/main/java/study/racingcar/Cars.java
@@ -0,0 +1,23 @@
+package study.racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private List<Car> cars;
+
+    public Cars(String namesWithComma) {
+        this.cars = addCars(namesWithComma.split(","));
+    }
+
+    private List<Car> addCars(String[] names) {
+        return Arrays.stream(names)
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+}
--- src/test/java/study/racingcar/RacingCarTest.java
+++ src/test/java/study/racingcar/RacingCarTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -11,15 +13,24 @@ public class RacingCarTest {
 
     @Test
     @DisplayName("각 자동차에 이름을 부여할 수 있다.")
-    void setCarName() {
+    void 자동차_이름_부여() {
         Car car = new Car("carA");
         assertThat(car.getName()).isEqualTo("carA");
     }
 
     @Test
     @DisplayName("자동차 이름은 5자를 초과할 수 없다.")
-    void carNameValidationTest() {
+    void 자동차_이름_5자_초과() {
         assertThatThrownBy(() -> new Car("666666"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름은 쉼표를 기준으로 구분한다.")
+    void 자동차_이름_쉼표_구분() {
+        Cars cars = new Cars("carA,carB,carC");
+        List<Car> carList = cars.getCars();
+        assertThat(carList).extracting("name")
+                .containsExactly("carA", "carB", "carC");
     }
 }
